e5034a278f71495f5e9fd9a5bdd6c9fa07a29c97
Gilbert Omosa <gilbert.omosa@student.moringaschool.com>
2023-11-27 13:00:00 +0300

    Done with Assignment

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,18 @@
-# your code goes here!
+def countdown(n): 
+    
+    while n > 0:
+        print(f"{n} SECOND(S)!")
+        n -= 1
+    print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(n): 
+    import time
+
+    while n > 0:
+        print(f"{n} SECOND(S)!")
+        n -= 1
+        time.sleep(1)
+    print("HAPPY NEW YEAR!")
+
+
+print(countdown_with_sleep(15))
